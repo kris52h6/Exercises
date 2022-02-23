f086a11dff5bff819b46b544aea3c3c9805f72a2
kris52h6 <kris52h6@stud.kea.dk>
2022-02-23 13:42:51 +0100

    zipcode

--- ZipCode/src/com/company/Main.java
+++ ZipCode/src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        HashMap<Integer, String> cityHashMap = new HashMap();
+	    File citiesData = new File("src/com/company/data/danske-postnumre-byer-ny.csv");
+
+        try {
+            Scanner sc = new Scanner(citiesData);
+            sc.nextLine();
+
+            while (sc.hasNext()) {
+                String line = sc.nextLine();
+                String[] lineSplit = line.split(";");
+                if (!sc.nextLine().equals(";")) {
+                    int zipCode = Integer.parseInt(lineSplit[0]);
+                    String cityName = lineSplit[1];
+
+                    cityHashMap.put(zipCode, cityName);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.print("Input zipcode: ");
+
+        Scanner userInput = new Scanner((System.in));
+        int cityUserInput = userInput.nextInt();
+        System.out.println(cityHashMap.get(cityUserInput));
+
     }
 }
